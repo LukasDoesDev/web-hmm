64ceb377cb5f30ea1b05656b3f5d1b138902202c
LukasDoesDev <lukasdoesdev@gmail.com>
2020-10-22 22:12:29 +0300

    ✅ Add dynamic method checking

--- test/tester.js
+++ test/tester.js
@@ -2,19 +2,22 @@ var assert = require("assert");
 const app = require("../src/app.js");
 const axios = require("axios").default;
 
+const methods = ['GET'];
+// const methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'PATCH'];
+
 describe("Test server", function () {
   before(function (done) {
-    app.methods.POST("/", (req, res, next) => {
-      res.sendPlain("Hello from POST");
-    });
     app.methods.GET("/", (req, res, next) => {
       res.sendPlain("Hello from GET");
     });
-    app.methods.PATCH("/", (req, res, next) => {
-      res.sendPlain("Hello from PATCH");
+    app.methods.POST("/", (req, res, next) => {
+      res.sendPlain("Hello from POST");
     });
     app.methods.DELETE("/", (req, res, next) => {
       res.sendPlain("Hello from DELETE");
+    });
+    app.methods.PATCH("/", (req, res, next) => {
+      res.sendPlain("Hello from PATCH");
     });
 
     app.createServer();
@@ -23,13 +26,16 @@ describe("Test server", function () {
   
   after(app.shutdown);
 
-  it("Test GET method with axios", async function () {
-    var response = await axios({
-      method: "get",
-      url: "http://localhost:3000/",
-      timeout: 1000
+  for (let i = 0; i < methods.length; i++) {
+    const method = methods[i]; // Current method
+    it(`Test ${method} method with axios`, async function () {
+      var response = await axios({
+        method: method,
+        url: "http://localhost:3000/",
+        timeout: 1000,
+      });
+      assert.strictEqual(response.data, `Hello from ${method}`);
+      assert.strictEqual(response.status, 200);
     });
-    assert.equal(response.data, `Hello from GET`);
-    assert.equal(response.status, 200);
-  });
+  }
 });
