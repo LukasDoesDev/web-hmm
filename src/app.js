a56816792b92056526e8868417e01201e3f5fdd6
LukasDoesDev <lukasdoesdev@gmail.com>
2020-10-22 22:56:49 +0300

    🚧 Adding support for creating multiple instances.

--- src/app.js
+++ src/app.js
@@ -1,57 +1,38 @@
-'use strict';
+"use strict";
 
-const http = require('http');
-
-const methods = require('./methods.js');
-const utils = require('./utils.js');
-const router = require('./router.js');
-const middleware = require('./middleware.js');
-const enums = require('./enums.js');
-var mode = enums.Modes.PRODUCTION;
-
-var server;
-
+const http = require("http");
 
 /**
  * Create new instance of the app
  */
 function createApp() {
   // todo
-  return {
-    createServer,
-    methods,
-    utils,
-    listen,
-    Router: router.Router,
-    useRouter: router.useRouter,
-    middleware,
-    debugMode() { mode = enums.Modes.DEBUG; },
-    shutdown
-  };
-}
 
-function createServer (config) {
+  const methods = require("./methods.js");
+  const utils = require("./utils.js");
+  const router = require("./router.js");
+  const middleware = require("./middleware.js");
+  const enums = require("./enums.js");
+  var mode = enums.Modes.PRODUCTION;
 
-  console.log(mode, enums.Modes);
-  if (
-    !(mode == enums.Modes.PRODUCTION
-    || mode == enums.Modes.DEBUG)
-  ) {
-    throw new Error('mode isn\'t enums.Modes.PRODUCTION or enums.Modes.DEBUG');
-  }
-  console.log(
-    `${mode == enums.Modes.PRODUCTION
-      ? 'PRODUCTION'
-      : 'DEBUG'} Mode`
-  );
-  console.log('Routers:', router.routers);
+  var server;
 
-  server = http.createServer((req, res) => {
-    console.log("Got connection");
-    middleware.run(req, res);
+  function createServer(config) {
+    console.log(mode, enums.Modes);
+    if (!(mode == enums.Modes.PRODUCTION || mode == enums.Modes.DEBUG)) {
+      throw new Error("mode isn't enums.Modes.PRODUCTION or enums.Modes.DEBUG");
+    }
+    console.log(
+      `${mode == enums.Modes.PRODUCTION ? "PRODUCTION" : "DEBUG"} Mode`
+    );
+    console.log("Routers:", router.routers);
 
-    // ! that was actually wrong 👇👇👇
-    /*if (methods.routes.hasOwnProperty(req.method)) {
+    server = http.createServer((req, res) => {
+      console.log("Got connection");
+      middleware.run(req, res);
+
+      // ! that was actually wrong 👇👇👇
+      /*if (methods.routes.hasOwnProperty(req.method)) {
       var route = findRoute(methods.routes[req.method], req.url);
       var routerRoute = router.findRouterRoute(req.method, req.url);
 
@@ -103,28 +84,44 @@ function createServer (config) {
       ));
 
     }/**/
-  }, config);
-}
+    }, config);
+  }
 
-/**
- * Listen on the port and call the callback when it's listening
- * @param {Number} port The port where the server would listen on
- * @param {function} callback The callback the server will call when it's listening
- */
-function listen (port, callback, config) { server.listen(port, callback); }
+  /**
+   * Listen on the port and call the callback when it's listening
+   * @param {Number} port The port where the server would listen on
+   * @param {function} callback The callback the server will call when it's listening
+   */
+  function listen(port, callback, config) {
+    server.listen(port, callback);
+  }
 
-/**
- * Shuts down the server with a nice message 👍
- * @param {function} callback
- */
-function shutdown(callback) {
-  console.log('Shutting Server Down ⏹');
-  server.close(callback);
-  console.log(`Shutted Server Down,
+  /**
+   * Shuts down the server with a nice message 👍
+   * @param {function} callback
+   */
+  function shutdown(callback) {
+    console.log("Shutting Server Down ⏹");
+    server.close(callback);
+    console.log(`Shutted Server Down,
 should automatically exit 😀`);
-}
+  }
 
+  return {
+    createServer,
+    methods,
+    utils,
+    listen,
+    Router: router.Router,
+    useRouter: router.useRouter,
+    middleware,
+    debugMode() {
+      mode = enums.Modes.DEBUG;
+    },
+    shutdown,
+  };
+}
 
 module.exports = {
-  createApp
+  createApp,
 };
